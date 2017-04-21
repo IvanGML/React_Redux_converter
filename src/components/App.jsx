4b4a35c60e4da18ea5c7c9f6938aef98f77f4648
IvanGML <somovlife@gmail.com>
2017-04-21 18:07:11 +0300

    base strucrure of converter UI designed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,18 +10,61 @@ const Title = styled.h1`
 
 
 const Wrapper = styled.section`
-  {
+{
   background: papayawhip;
   padding: 4em;
-}
-::after{
-  content: 'Ivan Somau production';
-  font-size: 10px;
-  position: absolute;
-  top: 0;
-  left: 20%;
+  min-height: 100vh;
 }
 `;
+
+const Input = styled.input`
+&:read-only{
+  background-color: #efefef;
+}
+{
+  margin: 20px 0 20px 20px;
+  display: block;
+  width: 300px;
+  font-family: sans-serif;
+  font-size: 18px;
+  appearance: none;
+  border-radius: none;
+  transition: border 0.3s;
+  outline: none;
+  padding: 10px;
+  padding-top: 11px;
+  transition: 0.3s;
+  border: none;
+  border-bottom: solid 2px #c9c9c9;
+}
+&:focus{
+  border-bottom: solid 2px #969696;
+}
+`
+
+const FlexContainer = styled.div`
+  {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Select = styled.select`
+  margin: 20px 20px 20px 0;
+  display: block;
+  font-family: sans-serif;
+  font-size: 18px;
+  appearance: none;
+  border-radius: none;
+  transition: border 0.3s;
+  outline: none;
+  padding: 10px;
+  transition: 0.3s;
+  border: none;
+  border-bottom: solid 2px #c9c9c9;
+`
+
 
 
 class App extends Component {
@@ -29,8 +72,37 @@ class App extends Component {
   render() {
     return (
     <Wrapper>
-      <Title>Converter is not working yet
-        <br/> :'|</Title>
+      <Title>Converter is not working yet :'|</Title>
+        <FlexContainer>
+          <Input type='number' placeholder='Put in'></Input>
+          <Select>
+            <option>one</option>
+            <option>two</option>
+            <option>three</option>
+          </Select>
+        </FlexContainer>
+        <FlexContainer>
+          <Input type='text' readOnly placeholder='Put out'></Input>
+          <Select>
+            <option>one</option>
+            <option>two</option>
+            <option>three</option>
+          </Select>
+        </FlexContainer>
+        <FlexContainer>
+          <span>Rate</span>
+          <Select>
+            <option>one</option>
+            <option>two</option>
+            <option>three</option>
+          </Select>
+          <Select>
+            <option>one</option>
+            <option>two</option>
+            <option>three</option>
+          </Select>
+        </FlexContainer>
+        
     </Wrapper>
     );
   }
