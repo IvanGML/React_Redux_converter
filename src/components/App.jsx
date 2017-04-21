7b4045281d5f2f834b07b2cc50b155ea363e005f
IvanGML <somovlife@gmail.com>
2017-04-21 12:03:48 +0300

    one more start commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Select from './select.jsx'
-import InputCustomRate from './inputCustomRate.jsx'
-import Input from './input.jsx'
-import Output from './output.jsx'
-// import Button from './button.jsx'
-import Dropdown from './dropdown.jsx'
-import MeasurementSelect from './measurementSelect.jsx';
 
 class App extends Component {
   
   render() {
     return (
       <div>
-        <div>
-          <Input/>
-          {this.props.globalStore.changeTOM === 'Specified' ? <MeasurementSelect/> : null}
-        </div>
-        <div>
-          <Output/>
-          {this.props.globalStore.changeTOM === 'Specified' ? <MeasurementSelect/> : null}
-        </div>
-        {this.props.globalStore.changeTOM === 'Custom' ? <InputCustomRate/> 
-          : this.props.globalStore.changeTOM === 'Specified' ? <Select/> 
-          : null }
-        <Dropdown/>
-        {/*<Button/>*/}
+          re-re
       </div>
     );
   }
--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,16 +1,6 @@
 import {combineReducers} from 'redux';
-import Dropdown from './dropdown.jsx'
 import changeTOM from './changeTOM.jsx';
-import Select from './select.jsx';
-import finalSelectValuesState from './finalSelectValuesState.jsx';
-import inputCustomRate from './inputCustomRate.jsx';
-import input from './input.jsx';
 
 export default combineReducers({
-    Dropdown,
-    changeTOM,
-    Select,
-    finalSelectValuesState,
-    inputCustomRate,
-    input
+    changeTOM
 })
