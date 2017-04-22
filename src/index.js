6b3ff7d8f9a48614e229ca8a945168c5bcf89d03
IvanGML <somovlife@gmail.com>
2017-04-22 17:39:11 +0300

    another one little commit ^_-

--- src/index.js
+++ src/index.js
@@ -16,42 +16,42 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-export let data = [
-  {
-    name: 'mm',
-    index: 1
-  }, {
-    name: 'm',
-    index: 1000
-  }, {
-    name: 'km',
-    index: 1000000
-  }, {
-    name: 'g',
-    index: 1
-  }, {
-    name: 'kg',
-    index: 1000
-  }, {
-    name: 't',
-    index: 1000000
-  }
-]
+// export let data = [
+//   {
+//     name: 'mm',
+//     index: 1
+//   }, {
+//     name: 'm',
+//     index: 1000
+//   }, {
+//     name: 'km',
+//     index: 1000000
+//   }, {
+//     name: 'g',
+//     index: 1
+//   }, {
+//     name: 'kg',
+//     index: 1000
+//   }, {
+//     name: 't',
+//     index: 1000000
+//   }
+// ]
 
-export let converter = (inputData, data) => {
-  let convertFrom = 0;
-  let convertTo = 0;
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].name===inputData[1]) {
-      convertFrom = data[i].index;
-    }
-  }
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].name===inputData[2]) {
-      convertTo = data[i].index;
-    }
-  }
-  return inputData[0] * convertFrom / convertTo;
-}
+// export let converter = (inputData, data) => {
+//   let convertFrom = 0;
+//   let convertTo = 0;
+//   for (let i = 0; i < data.length; i++) {
+//     if (data[i].name===inputData[1]) {
+//       convertFrom = data[i].index;
+//     }
+//   }
+//   for (let i = 0; i < data.length; i++) {
+//     if (data[i].name===inputData[2]) {
+//       convertTo = data[i].index;
+//     }
+//   }
+//   return inputData[0] * convertFrom / convertTo;
+// }
 
-console.log(converter([22,'m','km'],data));
+// console.log(converter([22,'m','km'],data));
